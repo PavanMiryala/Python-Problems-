0cc42300c10e71da2655522692633a369d51d177
PavanMiryala <pavanmiryala354@gmail.com>
2026-06-22 21:04:57 +0530

    commiting problems

---
+++ problems.py
@@ -0,0 +1,12 @@
+s = "a10b2c2"
+result = ""
+i = 0
+while i < len(s):
+    ch = s[i]
+    i += 1
+    num = ""
+    while i < len(s) and s[i].isdigit():
+        num += s[i]
+        i += 1
+    result += ch * int(num)
+print(result)
