d18d4a80965b24ac6de73dbbf82f90961686075f
PavanMiryala <pavanmiryala354@gmail.com>
2026-08-24 21:59:56 +0530

    started dsa

---
+++ first.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count={}
+        for x in s:
+            count[x]=count.get(x,0)+1
+     
+        for x in t:
+            count[x]=count.get(x,0)-1
+ 
+
+    
+        for x in count:
+            if count[x]!=0:
+                return False
+            
+        return True
