49b9a4935b72a997e6be79e448947002532204fa
PavanMiryala <pavanmiryala354@gmail.com>
2026-09-04 21:25:46 +0530

    practicing

--- problems.py
+++ problems.py
@@ -10,3 +10,15 @@ while i < len(s):
         i += 1
     result += ch * int(num)
 print(result)
+s = "a10b2c2"
+result = ""
+i = 0
+while i < len(s):
+    ch = s[i]
+    i += 1
+    num = ""
+    while i < len(s) and s[i].isdigit():
+        num += s[i]
+        i += 1
+    result += ch * int(num)
+print(result)
