8e7e0285c28eb9fde30cfed104062233615b5f5b
PavanMiryala <pavanmiryala354@gmail.com>
2026-09-14 22:40:30 +0530

    started problems from basic

--- first.py
+++ first.py
@@ -9,3 +9,17 @@ class Solution:
             if count[x]!=0:
                 return False 
         return True
+
+    
+s = "a10b2c2"
+result = ""
+i = 0
+while i < len(s):
+    ch = s[i]
+    i += 1
+    num = ""
+    while i < len(s) and s[i].isdigit():
+        num += s[i]
+        i += 1
+    result += ch * int(num)
+print(result)
